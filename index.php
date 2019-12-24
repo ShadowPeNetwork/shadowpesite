44e92a9a59c4f2a359f36a9a78fb6e2a8cebda87
ShadowPeNetwork <59189321+ShadowPeNetwork@users.noreply.github.com>
2019-12-23 22:27:50 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+<title>ShadowPe Network</title>
+  </head>
+  <h1>The Website Is Being Developed By SaltyBunnyRR1#500! If You Find Any Bugs, Please Reach Out To Him On Discord.</h1>
+    
+
+
+<form action="/apply/form.php" method="post">
+    <input type="submit" value="Apply For Staff!" 
+         name="Submit" id="frm1_submit" />
+</form>
+    <iframe src="https://discordapp.com/widget?id=649889059962880001&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+</html>
