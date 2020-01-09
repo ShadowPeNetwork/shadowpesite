2ff7bc61c4239ed20e599dd55494ed5e0cb655f1
SaltyBunny12772 <masen0914@gmail.com>
2020-01-09 16:40:36 -0600

    Signup/DB Stuff

---
+++ test2/includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername = "remotemysql.com,3306";
+$dBUsername = "Rj3FUTH3mU";
+$dBPassword = "GCnU1xWnfR";
+$dBName = "Rj3FUTH3mU";
+
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+if(!conn) {
+    die("Connection Failed: ".mysqli_connect_error());
+}
---
+++ test2/includes/signup.inc.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_POST['signup-submit'])) {
+
+require 'dbh.inc.php';
+
+
+}
