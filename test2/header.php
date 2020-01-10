2e4c43642d911009f39ffbcb53b33c6852d022c3
SaltyBunny12772 <masen0914@gmail.com>
2020-01-10 14:41:46 -0600

    Update

--- test2/header.php
+++ test2/header.php
@@ -10,7 +10,7 @@ session_start();
       <link rel="stylesheet" type="text/css" href="css/style.css" />
   </head>
   <body>
-  
+
     <header>
       <nav>
         <a href="#">
@@ -18,22 +18,22 @@ session_start();
         </a>
         <ul>
           <li><a href="index.php">Home</a></li>
-          <li><a href="#">Portfolio</a></li> 
+          <li><a href="#">Portfolio</a></li>
           <li><a href="#">About Me</a></li>
-          <li><a href="#">Contact</a></li>          
+          <li><a href="#">Contact</a></li>
         </ul>
         <div>
           <?php
             if (isset($_SESSION['userId'])) {
-            echo '<form action="includes/logout.inc.php" method="post"> 
+            echo '<form action="includes/logout.inc.php" method="post">
             <button type="submit" name="logout-submit" class="button">Logout</button>
           </form>';
             } else {
               echo '
-          <form action="includes/login.inc.php" method="post"> 
+          <form action="includes/login.inc.php" method="post">
              <input type="text" name="mailuid" placeholder="Username/ E-Mail">
             <input type="password" name="pwd" placeholder="Password">
-            <button type="submit" name="login-submit">Login</button>
+            <button type="submit" name="login-submit" class="button">Login</button>
           </form>
             <a href="signup.php" class="button">Sign Up</a>
             ';
