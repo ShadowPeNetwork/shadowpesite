e111917efa8ca5edd163a0444ee46c334fdec9cb
SaltyBunny12772 <masen0914@gmail.com>
2020-01-10 13:09:04 -0600

    update signup.inc.php

--- test2/includes/signup.inc.php
+++ test2/includes/signup.inc.php
@@ -22,18 +22,18 @@ if (empty($username) || empty($email) || empty($password) || empty($passwordRepe
     header("location: ../signup.php?error=invalidmail&uid=".$username);
     exit();
 } else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-    header("location: ../signup.php?error=invaliduid&mail=".$email);
+    header("location: ../signup.php?error=invaliduid&maild=".$email);
     exit();
 } else if ($password !== $passwordRepeat) {
     header("location: ../signup.php?error=passwordcheck&mail=".$email."&uid=".$username);
     exit();
 } else {
-    
+
     $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../signup.php?error=sqlerror");
-        
+
         exit();
     } else {
     mysqli_stmt_bind_param($stmt, "s", $username);
@@ -41,21 +41,21 @@ if (empty($username) || empty($email) || empty($password) || empty($passwordRepe
     mysqli_stmt_store_result($stmt);
     $resultCheck = mysqli_stmt_num_rows($stmt);
     if ($resultCheck > 0) {
-        header("location: ../signup.php?error=usertaken&mail=".$email);
-        exit();        
+        header("location: ../signup.php?error=usertaken&mailuid=".$email);
+        exit();
     } else {
-    
+
     $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-        
+
         header("location: ../signup.php?error=sqlerror");
         exit();
     } else {
         $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
 
         mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
-        mysqli_stmt_execute($stmt); 
+        mysqli_stmt_execute($stmt);
         header("location: ../signup.php?signup=success");
         exit();
     }
@@ -67,5 +67,5 @@ mysqli_close($conn);
 
 } else {
     header("location: ../signup.php");
-    exit();   
+    exit();
 }
